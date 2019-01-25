1ecf800acd13b6c0f8f8b9adf351500f27ec5833
Ratatouille <307338947@qq.com>
2019-01-25 22:40:31 +0800

    test

--- 01_Http服务器.py
+++ 01_Http服务器.py
@@ -13,12 +13,14 @@ def serve_client(new_socket: socket.socket):
 
     # 2. 返回http格式的数据
     # 2.1 准备给浏览器的数据 header
-    responce = "HTTP /1.1 200 OK \r\n"
+    responce = "HTTP/1.1 200 OK\r\n"
     responce += "\r\n"
 
     # 2.2 准备给浏览器的数据 body
-    responce += "hahahahaha"
+    responce += "<h2>hahahahaha</h2>"
+    print(responce)
     new_socket.send(responce.encode("utf-8"))
+    print("---sent---")
     new_socket.close()
 
 
@@ -26,9 +28,10 @@ def main():
     # 1. 创建套接字
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 2. 绑定
-    tcp_server_socket.bind(("", 6666))
+    tcp_server_socket.bind(("192.168.31.79", 7897))
     # 3. 设置为监听
     tcp_server_socket.listen(128)
+
 
     # 4. 等待新客户
     while True:
